1741963c5157ec6201fb10bc8e99e467424a6d99
Tiago Rodrigues <tmcrodrigues@gmail.com>
2014-02-04 22:14:59 +0100

    js fix

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@
   var showComments = document.querySelectorAll('.comments-cover button');
   var comments = document.querySelectorAll('.comments');
   
-  showComments[0].addEventListener('click', function (ev) {
+  showComments.length > 0 && showComments[0].addEventListener('click', function (ev) {
     commentsCover[0].className += ' hidden';
     comments[0].className = comments[0].className.replace('hidden', '');
   }, false);
