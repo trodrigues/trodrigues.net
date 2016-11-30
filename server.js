982ee2069ad3b1e7130bec0574ef82ea1279b504
Tiago Rodrigues <tmcrodrigues@gmail.com>
2016-11-30 20:21:53 +0100

    Setup server templating

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const Hapi = require('hapi')
+const fs = require('fs')
 
 const PORT = process.env.NODE_ENV === 'production' ? 6011 : 6010
+const LAYOUT_FILE = 'index.html'
 
 const server = new Hapi.Server()
 server.connection({ port: PORT })
@@ -9,7 +11,16 @@ server.route({
   method: 'GET',
   path: '/',
   handler: function (request, reply) {
-    reply('Hello, world!')
+    fs.readFile(LAYOUT_FILE, 'utf8', (err, file) => {
+      if (err) throw err
+      /* eslint-disable no-new-func */
+      const template = new Function('o', 'return `' + file + '`')
+      /* eslint-enable no-new-func */
+      reply(template({
+        title: `Tiago's page`,
+        content: 'o hello there'
+      }))
+    })
   }
 })
 
