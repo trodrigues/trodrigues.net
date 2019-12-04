754af94cb32f12eebc20812df6f441539f25c37f
Tiago Rodrigues <tmcrodrigues@gmail.com>
2019-12-04 14:57:41 +0100

    fetch from top

--- worker-test/client.js
+++ worker-test/client.js
@@ -1,3 +1,11 @@
 const worker = new Worker('/worker-test/worker.js');
 
 worker.postMessage('hello from client')
+
+window.fetch('/').then(function(response){
+  return response.text().then(function(data){
+    console.log('response', data.replace(/\n/g, ''))
+  })
+}).catch(function(err) {
+  console.log('failed', err)
+})
