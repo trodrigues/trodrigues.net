de0cd7b78e7f1cfed6e5d845427d5bc9f5e2cc33
Tiago Rodrigues <tmcrodrigues@gmail.com>
2019-12-04 15:17:05 +0100

    just use xhr

--- worker-test/worker.js
+++ worker-test/worker.js
@@ -1,13 +1,10 @@
-if(!self.fetch) {
-  self.importScripts('https://polyfill.io/v3/polyfill.min.js?features=fetch%2CPromise')
-}
 self.addEventListener("message", function(ev) {
   console.log('post message data', ev.data)
-  self.fetch('/').then(function(response){
-    return response.text().then(function(data){
-      console.log('fetch response', data.replace(/\n/g, ''))
-    })
-  }).catch(function(err) {
-    console.log('fetch failed', err)
-  })
+  
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", function(){
+    console.log(this.responseText);
+  });
+  oReq.open("POST", "/");
+  oReq.send();
 });
